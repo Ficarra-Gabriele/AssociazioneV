e1d12cd9252927293c7ed982e9947114aa9c3274
Ficarra-Gabriele <ficarra.gabriele@franchettisalviani.net>
2025-11-04 10:44:13 +0100

    finito

---
+++ MainAssociazione/src/mainassociazione/MainAssociazione.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainassociazione;
+
+/**
+ *
+ * @author ficarra.gabriele
+ */
+public class MainAssociazione {
+    public static void main(String[] args) {
+        Docente docente1 = new Docente("Stefano", "Informatica");
+        Aula aula1 = new Aula("LAB-01", 28);
+        Aula aula2 = new Aula("AULA-12", 22);
+
+        // Associazione bidirezionale
+        docente1.assegnaAula(aula1); 
+        System.out.println(docente1);   //Docente{nome='Stefano', materia='Informatica', aula=LAB-01}
+        System.out.println(aula1);      //Aula{codice='LAB-01', posti=28, docente=Stefano}
+
+        // Cambio aula
+        docente1.assegnaAula(aula2);
+        System.out.println(docente1);   //Docente{nome='Stefano', materia='Informatica', aula=AULA-12}
+        System.out.println(aula1);      //Aula{codice='LAB-01', posti=28, docente=null}
+        System.out.println(aula2);      //Aula{codice='AULA-12', posti=22, docente=Stefano}
+
+        // Rimozione
+        docente1.rimuoviAula();
+        System.out.println(docente1);   //Docente{nome='Stefano', materia='Informatica', aula=null}
+        System.out.println(aula1);      //Aula{codice='LAB-01', posti=28, docente=null}
+        System.out.println(aula2);      //Aula{codice='AULA-12', posti=22, docente=null}    
+    }
+}
